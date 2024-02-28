7340235cbbbe6909d5bc6a28438a530b2a85041c
Yijia Chen <kevinc110604@gmail.com>
2024-02-28 10:41:56 -0500

    [new] L63, add routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
 import Header from './components/header';
+import Home from './pages/home'
+import Detail from "./pages/detail";
 
 function App() {
   return (
-    <>
-      <Provider
-        store={store}
-      >
-        <Header/>
-      </Provider>
-    </>
+    <Provider
+      store={store}
+    > 
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={<Home/>}/>
+          <Route path="/detail"  element={<Detail/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
