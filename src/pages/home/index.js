c2f1f11195ff89019a23ee2834d509f8184c2036
Yijia Chen <kevinc110604@gmail.com>
2024-02-28 22:53:17 -0500

    [new] L68, use axios to fetch data in home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,24 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import List from "./components/List";
 import Recommend from "./components/Recommned";
 import Topic from "./components/Topic";
 import Writer from "./components/Writer";
 import { HomeLeft, HomeRight, HomeWrapper } from "./style";
 
-function Home() {
-  return (
-    <HomeWrapper>
-      <HomeLeft>
-        <img className="banner-img" src="https://m.media-amazon.com/images/I/61GC3F6XpsL._SX3000_.jpg"/>
-        <Topic/>
-        <List/>
-      </HomeLeft>
-      <HomeRight>
-        <Recommend/>
-        <Writer/>
-      </HomeRight>
-    </HomeWrapper>
-  );
+class Home extends Component {
+  render() {
+    return (
+      <HomeWrapper>
+        <HomeLeft>
+          <img alt="" className="banner-img" src="https://m.media-amazon.com/images/I/61GC3F6XpsL._SX3000_.jpg"/>
+          <Topic/>
+          <List/>
+        </HomeLeft>
+        <HomeRight>
+          <Recommend/>
+          <Writer/>
+        </HomeRight>
+      </HomeWrapper>
+    );
+  }
+
+  componentDidMount() {
+    axios.get('/api/home.json').then((res) => {
+      const data = res.data;
+      const action = {
+        type: 'change_home_data',
+        topicList: data.topicList,
+        articleList: data.articleList, 
+        recommendList: data.recommendList,
+      };
+      this.props.handleHomeData(action);
+    })
+  }
 }
 
-export default Home;
+const mapStateToProps = null;
+// (state) => {
+//   return {
+//     articleList: state.getIn(['home','articleList']),
+//   }
+// }
+
+const mapDispatchToProps = (dispatch) => ({
+  handleHomeData(action) {
+    dispatch(action);
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
