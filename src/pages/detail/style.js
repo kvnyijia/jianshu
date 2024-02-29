415d5b0cab8ac908db9f4b0ceeba6eba38207bfd
Yijia Chen <kevinc110604@gmail.com>
2024-02-29 12:56:57 -0500

    [new] L73, style detail page

---
+++ src/pages/detail/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+
+export const DetailWrapper = styled.div`
+  overflow: hidden;
+  padding-bottom: 100px;
+  width: 620px;
+  margin: 0 auto;
+`;
+
+export const Header = styled.div`
+  margin: 50px 0 20px 0;
+  width: 620px;
+  line-height: 44px;
+  color: #333;
+  font-size: 44px;
+  font-weight: bold;
+`;
+
+export const Content = styled.div`
+  color: #2f2f2f;
+  img {
+    width: 100%;
+  }
+  p {
+    margin: 25px 0;
+    font-size: 16px;
+    line-height: 30px;
+  }
+  b {
+    font-weight: bold;
+  }
+  overflow: hidden;
+  padding-bottom: 100px;
+  width: 620px;
+  margin: 0 auto;
+`;
